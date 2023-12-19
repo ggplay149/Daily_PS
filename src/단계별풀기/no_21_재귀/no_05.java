4e0ee92526470966f944bec329ba5ebd0483d524
ggplay149 <ggplay149@gmail.com>
2023-12-19 22:12:19 +0900

    20231219

--- src/단계별풀기/no_21_재귀/no_05.java
+++ src/단계별풀기/no_21_재귀/no_05.java
@@ -1,5 +1,43 @@
 package 단계별풀기.no_21_재귀;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class no_05 {
     //칸토어 집합
+    static StringBuilder sb;
+
+    static String[] arr;
+
+    public static void main (String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while((str = br.readLine()) != null) {
+            sb = new StringBuilder();
+            int m = (int) Math.pow(3, Integer.parseInt(str));
+            arr = new String[m];
+            for (int j = 0; j < m; j++) {
+                arr[j] = "-";
+            }
+            draw(0,m);
+            for (String answer : arr) {
+                sb.append(answer);
+            }
+            sb.append("\n");
+            System.out.print(sb);
+
+        }
+    }
+    public static void draw(int start, int size) {
+        if (size == 1) {
+                return;
+        }
+        int plus = size/3;
+        for (int i = start+plus; i < start+2*plus; i++) {
+            arr[i] = " ";
+        }
+        draw(start,plus);
+        draw(start+2*plus,plus);
+    }
 }
---
+++ src/단계별풀기/no_23_동적계획법1/test.java
@@ -0,0 +1,43 @@
+package 단계별풀기.no_23_동적계획법1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test {
+
+    static Integer[] dp;
+
+    public static void main (String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new Integer[n+3];
+        dp[1]=0;
+        dp[2]=1;
+        dp[3]=1;
+
+        System.out.print(cal(n));
+
+    }
+    static int cal(int n){
+
+        if(dp[n]!=null) return dp[n];
+
+        if(n%2!=0 && n%3!=0) {
+            dp[n] = cal(n-1)+1;
+        }else {
+
+            int temp1 =1000;
+            int temp2 =1000;
+
+            if (n % 2 == 0) {
+                temp1 = Math.min(cal(n / 2) + 1, cal(n - 1) + 1);
+            }
+            if (n % 3 == 0) {
+                temp2 = Math.min(cal(n / 3) + 1, cal(n - 1) + 1);
+            }
+            dp[n] = Math.min(temp1,temp2);
+        }
+        return dp[n];
+    }
+}
