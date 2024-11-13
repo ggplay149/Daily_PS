7ef06830b50930052ba35f684a9df48bb6287486
syou149149 <syou149149@gmail.com>
2024-11-13 17:55:24 +0900

    2024_11_13 책 c1_Q4

--- src/book/chap2_array/거리두기확인하기.java
+++ src/book/chap2_array/거리두기확인하기.java
@@ -4,63 +4,67 @@ public class 거리두기확인하기 {
     private static int[] dx = {1,0,-1,0};
     private static int[] dy = {0,1,0,-1};
     private static char[][] board = new char[5][5];
+    private static int[] answer;
 
-    public static void main(String[] args){
+    public int[] solution(String[][] places) {
 
-        String[] input = {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"};
+        answer = new int[places.length];
 
-        for(int i = 0 ; i < 5 ; i ++) {
-            for(int j = 0 ; j < 5 ; j ++) {
-                board[i][j] = input[i].charAt(j);
-            }
-        }
+        n: for (int n = 0; n < places.length; n++) {
 
-        for(int i = 0 ; i < 5 ; i ++) {
-            for(int j = 0 ; j < 5 ; j ++) {
+            String[] words = places[n];
 
-                if(board[i][j]!='P') continue;
-
-                for(int d = 0 ; d <4 ; d ++){
-
-                    int nx = j+dx[d];
-                    int ny = i+dy[d];
-
-                    if(nx == 5 || ny == 5 || nx == -1 || ny == -1) continue;
-
-                    switch (board[ny][nx]){
-                        case 'O' :
-                            break;
-                        case 'X' :
-                            break;
-                        case 'P' :
-                            System.out.println("실패");
-                            break;
-                    }
-
-
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    board[i][j] = words[i].charAt(j);
                 }
+            }
 
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+
+                    if (board[i][j] != 'P') continue;
+
+                    for (int d = 0; d < 4; d++) {
+
+                        int nx = j + dx[d];
+                        int ny = i + dy[d];
+
+                        if (nx == 5 || ny == 5 || nx == -1 || ny == -1) continue;
+
+                        switch (board[ny][nx]) {
+                            case 'O':
+                                if (!checkDistanced(nx,ny,i,j)) {
+                                    answer[n] = 0;
+                                    continue n;
+                                }
+                                break;
+                            case 'X':
+                                break;
+                            case 'P':
+                                answer[n] = 0;
+                                continue n;
+                        }
+                    }
+                }
+                answer[n] = 1;
             }
         }
-
-
-        for(int i = 0 ; i < 5 ; i ++) {
-            for(int j = 0 ; j < 5 ; j ++) {
-               System.out.print(board[i][j]+" ");
-            }
-            System.out.println();
-        }
+        return answer;
     }
+    private static boolean checkDistanced ( int x, int y, int fromY, int fromX){
 
+        for (int i = 0; i < 4; i++) {
 
-    private static boolean checkDistanced(int x, int y){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        for(int i = 0 ; i <4 ; i ++){
+            if (nx == 5 || ny == 5 || nx == -1 || ny == -1) continue;
+            if(fromY == ny && fromX == nx) continue;
 
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-
-            if(board[ny][nx]=='P') return false;
+            if (board[ny][nx] == 'P') {
+                return false;
+            }
         }
 
         return true;
